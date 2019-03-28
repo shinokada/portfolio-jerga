53b8c0d807bb8b3dbbad43c37954642c27dc77e7
Shinichi Okada <okada.shin@gmail.com>
2019-03-29 07:02:24 +0900

    Update

--- pages/secret.js
+++ pages/secret.js
@@ -4,68 +4,68 @@ import BasePage from '../components/BasePage';
 
 import withAuth from '../components/hoc/withAuth';
 
-import { getSecretData, getSecretDataServer } from '../actions';
+import { getSecretData } from '../actions';
 
 class Secret extends React.Component {
 
-  static async getInitialProps({req}) {
-    const anotherSecretData =  await getSecretData(req);
+	static async getInitialProps({ req }) {
+		const anotherSecretData = await getSecretData(req);
 
-    return { anotherSecretData };
-  }
+		return { anotherSecretData };
+	}
 
-  // constructor(props) {
-  //   super();
+	// constructor(props) {
+	//   super();
 
-  //   this.state = {
-  //     secretData: []
-  //   }
-  // }
+	//   this.state = {
+	//     secretData: []
+	//   }
+	// }
 
-  state = {
-    secretData: []
-  }
+	state = {
+		secretData: []
+	}
 
-  async componentDidMount() {
-    const secretData = await getSecretData();
+	async componentDidMount() {
+		const secretData = await getSecretData();
 
-    this.setState({
-      secretData
-    });
-  }
+		this.setState({
+			secretData
+		});
+	}
 
-  displaySecretData() {
-    const { secretData } = this.state;
+	displaySecretData() {
+		const { secretData } = this.state;
 
-    if ( secretData && secretData.length > 0) {
-      return secretData.map((data, index) => {
-        return (
-          <div key={index}>
-            <p> { data.title }</p>
-            <p> { data.description }</p>
-          </div>
-        )
-      })
-    }
+		if (secretData && secretData.length > 0) {
+			return secretData.map((data, index) => {
+				return (
+					<div key={index}>
+						<p> {data.title}</p>
+						<p> {data.description}</p>
+					</div>
+				)
+			})
+		}
 
-    return null;
-  }
+		return null;
+	}
 
 
-  render() {
-    const { superSecretValue } = this.props;
+	render() {
+		const { superSecretValue } = this.props;
 
-    return (
-      <BaseLayout {...this.props.auth}>
-        <BasePage>
-          <h1> I am Secret Page </h1>
-          <p> Secret Content Here </p>
-          <h2> {superSecretValue} </h2>
-          { this.displaySecretData() }
-        </BasePage>
-      </BaseLayout>
-    )
-  }
+		return (
+			<BaseLayout {...this.props.auth}>
+				<BasePage>
+					<h1> I am Secret Page </h1>
+					<p> Secret Content Here </p>
+					<h2> {superSecretValue} </h2>
+					{this.displaySecretData()}
+				</BasePage>
+			</BaseLayout>
+		)
+	}
 }
 
 export default withAuth()(Secret);
